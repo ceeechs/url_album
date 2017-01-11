b88d536dbb9f839f0abc041c2f6eb9ffeba116fd
高城 晃一 <ktakashiro@geechs.com>
2017-01-11 21:10:30 +0900

    css読み込み順変更

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -4,8 +4,8 @@
 	<meta charset="UTF-8">
 	<meta name="robots" content="noimageindex">
 	<title><?php echo $title; ?></title>
-	<?php echo Asset::css(array("main.css", "comment.css", "bootstrap.css", "zoom.css")); ?>
-	<?php echo Asset::js(array("jquery-3.1.1.min.js", "jquery.lazyload.min.js", "bootstrap.min.js", "main.js", "zoom.js")); ?>
+	<?php echo Asset::css(array("bootstrap.css", "main.css", "comment.css", "zoom.css")); ?>
+	<?php echo Asset::js(array("jquery-3.1.1.min.js", "bootstrap.min.js", "jquery.lazyload.min.js", "zoom.js", "main.js")); ?>
 	<style>@import url(http://fonts.googleapis.com/earlyaccess/notosansjapanese.css);</style>
 </head>
 <body>
