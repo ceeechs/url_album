8a61bb043ae28571297402b6debfa7505e70b7e2
高城 晃一 <ktakashiro@geechs.com>
2017-01-22 20:26:13 +0900

    header修正中

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -9,41 +9,35 @@
 	<?php echo Asset::js(array("jquery-3.1.1.min.js", "bootstrap.min.js", "jquery.lazyload.min.js", "zoom.js", "main.js")); ?>
 	<!-- ,"jquery-ui.min.js","jquery.slicknav.min.js"  -->
 
-
-
-	<!-- drawer.css -->
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/drawer/3.1.0/css/drawer.min.css">
-	<!-- jquery & iScroll -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/iScroll/5.1.3/iscroll.min.js"></script>
-	<!-- drawer.js -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/drawer/3.1.0/js/drawer.min.js"></script>
-
-
-
-
 	<!-- フォント -->
 	<style>@import url(http://fonts.googleapis.com/earlyaccess/notosansjapanese.css);</style>
 
 	<script>
-		$(document).ready(function() {
-		$('.drawer').drawer();
-		});
+		// $(document).ready(function() {
+		// $('.drawer').drawer();
+		// });
 	</script>
 </head>
-<body class="drawer drawer--left">
-	<header role="banner">
-		<button type="button" class="drawer-toggle drawer-hamburger">
-		<span class="sr-only">toggle navigation</span>
-		<span class="drawer-hamburger-icon"></span>
-		</button>
-		<nav class="drawer-nav" role="navigation">
-		<ul class="drawer-menu">
-		<li><a class="drawer-brand" href="#">Brand</a></li>
-		<li><a class="drawer-menu-item" href="http://git.blivesta.com/drawer/">Nav1</a></li>
-		<li><a class="drawer-menu-item" href="#">Nav2</a></li>
-		</ul>
-		</nav>
-	</header>
+<body class="drawer">
+	<nav class="navbar navbar-default">
+	  <div class="navbar-header">
+	    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#gnavi">
+	      <span class="sr-only">メニュー</span>
+	      <span class="icon-bar"></span>
+	      <span class="icon-bar"></span>
+	      <span class="icon-bar"></span>
+	    </button>
+	    <a href="#" class="navbar-brand">URL-Album</a>
+	  </div>
+	 
+	  <div id="gnavi" class="collapse navbar-collapse">
+	    <ul class="nav navbar-nav">
+	      <li><a href="">Link1</a></li>
+	      <li><a href="">Link2</a></li>
+	      <li><a href="">Link3</a></li>
+	    </ul>
+	  </div>
+	</nav>
 	<!-- http://www.webdesignleaves.com/wp/jquery/1384/ -->
 	<div class="container">
 		<div class="row header">
