c699a44e045e4095cf0c1b1e9cf2cf3744cbe151
高城 晃一 <ktakashiro@geechs.com>
2017-01-22 15:53:42 +0900

    header修正中

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -13,37 +13,37 @@
 <body>
 	<!-- ヘッダーメニュー -->
 	<!-- http://www.webdesignleaves.com/wp/jquery/1384/ -->
+	<div>
+		<ul id="header-menu">
+		    <li>Parent 1
+		        <ul>
+		            <li><a href="#">item 3</a></li>
+		            <li>Parent 3
+		                <ul>
+		                    <li><a href="#">item 8</a></li>
+		                    <li><a href="#">item 9</a></li>
+		                    <li><a href="#">item 10</a></li>
+		                </ul>
+		            </li>
+		            <li><a href="#">item 4</a></li>
+		        </ul>
+		    </li>
+		    <li><a href="#">item 1</a></li>
+		    <li>non-link item</li>
+		    <li>Parent 2
+		        <ul>
+		            <li><a href="#">item 5</a></li>
+		            <li><a href="#">item 6</a></li>
+		            <li><a href="#">item 7</a></li>
+		        </ul>
+		    </li>
+		</ul>
+	</div>
 	<div class="container">
 		<div class="row header">
 			<div class="col-sm-12">
 				<!-- <button type="button" class="btn btn-default">Default</button> -->
 				<p>URL-ALBUM</p>
-			</div>
-			<div>
-				<ul id="header-menu">
-				    <li>Parent 1
-				        <ul>
-				            <li><a href="#">item 3</a></li>
-				            <li>Parent 3
-				                <ul>
-				                    <li><a href="#">item 8</a></li>
-				                    <li><a href="#">item 9</a></li>
-				                    <li><a href="#">item 10</a></li>
-				                </ul>
-				            </li>
-				            <li><a href="#">item 4</a></li>
-				        </ul>
-				    </li>
-				    <li><a href="#">item 1</a></li>
-				    <li>non-link item</li>
-				    <li>Parent 2
-				        <ul>
-				            <li><a href="#">item 5</a></li>
-				            <li><a href="#">item 6</a></li>
-				            <li><a href="#">item 7</a></li>
-				        </ul>
-				    </li>
-				</ul>
 			</div>
 		</div>
 	</div>
