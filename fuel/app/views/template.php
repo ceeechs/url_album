6d9908c3dd4bfca3bf06ae71d061528fa2c62402
高城 晃一 <ktakashiro@geechs.com>
2017-01-22 17:18:18 +0900

    header修正中

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -6,16 +6,19 @@
 	<title><?php echo $title; ?></title>
 	<?php echo Asset::css(array("bootstrap.css", "main.css", "comment.css", "zoom.css")); ?>
 	<!-- , "slicknav.css" -->
-	<?php echo Asset::js(array("jquery-3.1.1.min.js", "bootstrap.min.js", "jquery.lazyload.min.js", "zoom.js", "main.js")); ?>
-	<!-- ,"jquery-ui.min.js","jquery.slicknav.min.js"  -->
+	<?php echo Asset::js(array( "bootstrap.min.js", "jquery.lazyload.min.js", "zoom.js", "main.js")); ?>
+	<!--"jquery-3.1.1.min.js", ,"jquery-ui.min.js","jquery.slicknav.min.js"  -->
 
 	<!-- フォント -->
 	<style>@import url(http://fonts.googleapis.com/earlyaccess/notosansjapanese.css);</style>
+
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 
 	<script>
 		// $(function(){
 		// 	$('#headr-menu').slicknav();
 		// });
+
 		$(function() {
 		    $('#navToggle').click(function(){//headerに .openNav を付加・削除
 		        $('header').toggleClass('openNav');
@@ -25,21 +28,21 @@
 </head>
 <body>
 	<header>
-	<h1>ロゴ<!-- <a href="index.html"><img src="img/header_logo.png"></a> --></h1>
-	<div id="navToggle">
-	  <div>
-	   <span></span> <span></span> <span></span>
-	  </div>
-	</div><!--#navToggle END-->
-	<nav>
-	  <ul>
-	   <li><a href="#">HOME</a></li>
-	   <li><a href="#">MENU1</a></li>
-	   <li><a href="#">MENU2</a></li>
-	   <li><a href="#">MENU3</a></li>
-	   <li><a href="#">MENU4</a></li>
-	  </ul>
-	</nav>
+		<h1>ロゴ<!-- <a href="index.html"><img src="img/header_logo.png"></a> --></h1>
+		<div id="navToggle">
+		  <div>
+		   <span></span> <span></span> <span></span>
+		  </div>
+		</div><!--#navToggle END-->
+		<nav>
+		  <ul>
+		   <li><a href="#">HOME</a></li>
+		   <li><a href="#">MENU1</a></li>
+		   <li><a href="#">MENU2</a></li>
+		   <li><a href="#">MENU3</a></li>
+		   <li><a href="#">MENU4</a></li>
+		  </ul>
+		</nav>
 	</header>
 	<!-- ヘッダーメニュー -->
 	<!-- <ul id="headr-menu">
