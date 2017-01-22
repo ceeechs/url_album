a5e42c8f5f8af21a1d01191e13c6ed535f783d50
高城 晃一 <ktakashiro@geechs.com>
2017-01-22 21:33:39 +0900

    header修正中

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="robots" content="noimageindex">
 	<title><?php echo $title; ?></title>
-	<?php echo Asset::css(array("bootstrap.min.css", "main.css", "comment.css", "zoom.css")); ?>
+	<?php echo Asset::css(array("bootstrap.css", "main.css", "comment.css", "zoom.css")); ?>
 	<!-- , "slicknav.css" -->
 	<?php echo Asset::js(array("jquery-3.1.1.min.js", "bootstrap.min.js", "jquery.lazyload.min.js", "zoom.js", "main.js")); ?>
 	<!-- ,"jquery-ui.min.js","jquery.slicknav.min.js"  -->
