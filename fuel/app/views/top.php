3e8e1559d32398dbf74b11048142d05f72dbd18c
高城 晃一 <ktakashiro@geechs.com>
2017-02-06 23:49:48 +0900

    メニュー実験

--- fuel/app/views/top.php
+++ fuel/app/views/top.php
@@ -19,7 +19,7 @@
 
 		<?php if ( $content_no == 0 || $prev_year_month != $year_month ): ?>
 				<script>
-					$('#nav_ul').appendChild('<li><a href="#<?php echo $year_month;?>"><?php echo $year.'年'.$month.'月';?></a></li>');
+					$('#nav_ul').append('<li><a href="#<?php echo $year_month;?>"><?php echo $year.'年'.$month.'月';?></a></li>');
 				</script>
 				<div id="<?php echo $year_month; ?>" class="row">
 					<p class="date">
